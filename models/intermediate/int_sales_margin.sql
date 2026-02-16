124beb16cbcaace908ebd417e456142722a5a440
Sabri Can Yilmaz <yilmaz.sabrican@outlook.com>
2026-02-16 17:42:12 +0000

    XXX

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,8 +1,8 @@
  
 
  select *,
-        ROUND(sales.quantity * product.purchase_price,2) as maliyet,
-        ROUND( sales.revenue - (sales.quantity * product.purchase_price),2 ) as margin
+        ROUND(sales.quantity * product.purchase_price,2) as purchase_cost,
+        ROUND(sales.revenue - (sales.quantity * product.purchase_price),2 ) as margin
  from {{ ref("stg_raw__sales")}} as sales
  left join {{ ref("stg_raw__product")}} as product 
  on sales.product_id = product.products_id
